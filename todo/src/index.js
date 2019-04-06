870f3baa63e58fa7cc08b7f78c424a24e5e92adf
vklimov <vklimov@livemaster.ru>
2019-04-06 13:39:02 +0300

    test react

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const elem = (
+	<div>
+		<h1>Hello world</h1>
+		<input placeholder="search" />
+		<ul>
+			<li>Learn React</li>
+			<li>Build Awesome App</li>
+		</ul>
+	</div>
+);
+	
+ReactDOM.render(elem, document.getElementById('root'));
