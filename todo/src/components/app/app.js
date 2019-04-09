872206ca55485eb93c20d778369740ba5914e6e4
vklimov <vklimov@livemaster.ru>
2019-04-09 22:01:17 +0300

    changed functions on classes

--- todo/src/components/app/app.js
+++ todo/src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -6,24 +6,25 @@ import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
 import './app.css';
 
-const App = () => {
-	const todos = [
-		{ label: 'Drink coffee', important: false, id: 2343 },
-		{ label: 'Make awesome app', important: true, id: 24352 },
-		{ label: 'Have a lunch', important: false, id: 908342 },
-	];
+export default class App extends Component {
 
-	return (
-		<div className="todo-app">
-			<AppHeader toDo={1} done={3} />
-			<div className="top-panel d-flex">
-				<SearchPanel />
-				<ItemStatusFilter />
+	render() {
+		this.todos = [
+			{ label: 'Drink coffee', important: false, id: 2343 },
+			{ label: 'Make awesome app', important: true, id: 24352 },
+			{ label: 'Have a lunch', important: false, id: 908342 },
+		];
+
+		return (
+			<div className="todo-app">
+				<AppHeader toDo={1} done={3} />
+				<div className="top-panel d-flex">
+					<SearchPanel />
+					<ItemStatusFilter />
+				</div>
+
+				<TodoList todos={this.todos} />
 			</div>
-
-			<TodoList todos={todos} />
-		</div>
-	);
-};
-
-export default App;
+		);
+	}
+}
--- todo/src/components/todo-list/todo-list.js
+++ todo/src/components/todo-list/todo-list.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TodoListItem from '../todo-list-item';
 import './todo-list.css';
 
-const TodoList = ({ todos }) => {
-	const elements = todos.map((item) => {
-		const { id, ...props } = item;
+export default class TodoList extends Component {
+	render() {
+		const elements = this.props.todos.map((item) => {
+			const { id, ...props } = item;
+			return (
+				<li key={id} className="list-group-item">
+					<TodoListItem { ...props }/>
+				</li>
+			);
+		});
+
 		return (
-			<li key={id} className="list-group-item">
-				<TodoListItem { ...props }/>
-			</li>
+			<ul className="list-group todo-list">
+				{ elements }
+			</ul>
 		);
-	});
-
-	return (
-		<ul className="list-group todo-list">
-			{ elements }
-		</ul>
-	);
-};
-
-export default TodoList;
+	}
+}
